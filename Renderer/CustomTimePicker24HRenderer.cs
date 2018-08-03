c5a20639d0c9907bf232c0ae4617436c7f2b3b28
Stephan Blaurock <stephan.blaurock@outlook.de>
2018-08-03 10:57:31 +0200

    Erstes Einchecken des Moduls Ginkgo.iOS

---
+++ Renderer/CustomTimePicker24HRenderer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Foundation;
+using Ginkgo.Views;
+using Ginkgo.iOS.Renderer;
+
+[assembly: ExportRenderer(typeof(CustomTimePicker24H), typeof(CustomTimePicker24HRenderer))]
+namespace Ginkgo.iOS.Renderer {
+	public class CustomTimePicker24HRenderer : TimePickerRenderer {
+		protected override void OnElementChanged(ElementChangedEventArgs<TimePicker> e) {
+			base.OnElementChanged(e);
+			var timePicker = (UIDatePicker)Control.InputView;
+			timePicker.Locale = new NSLocale("no_nb");
+			if (Control != null) {
+				Control.Text = DateTime.Now.ToString("HH:mm");
+			}
+		}
+	}
+
+}
